b4a5f2d6ee9cadc7d979f4e36a2162b800afc368
sciatti <sciatti@altair.com>
2025-01-04 22:58:24 -0500

    moving work around

--- 16/puzzle1/sln.py
+++ 16/puzzle1/sln.py
@@ -2,39 +2,45 @@ from __future__ import annotations
 from typing import TypedDict
 from dataclasses import dataclass
 from collections import deque
+from queue import PriorityQueue
+
+search_dirs: list[tuple[int, int]] = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 class Node(TypedDict):
+    orientation: str # U/D/L/R for up/down/left/right
     indices: tuple[int, int]
-    edges: list[Edge]
-
-class Edge(TypedDict):
-    value: int
 
 @dataclass
 class PuzzleInput():
     grid: list[list[str]]
     start: tuple[int, int]
     end: tuple[int, int]
-    graph: list[Node]
 
-def parse_input(file_handler):
+def parse_input(file_handler) -> PuzzleInput:
     puzzle_input: PuzzleInput = PuzzleInput(grid=[], start=(0, 0), end=(0, 0), graph=[])
     for line in file_handler.readlines():
         puzzle_input.grid.append(list(line.rstrip()))
     puzzle_input.start = (len(puzzle_input.grid) - 2, 1)
     puzzle_input.end = (1, len(puzzle_input.grid[0]) - 2)
-    
-    # up/right/down/left
-    search_dirs: list[tuple[int, int]] = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-    stack: deque[tuple[int, int]] = deque([puzzle_input.start])
-    added_nodes: set[tuple[int, int]] = set([puzzle_input.start])
-    puzzle_input.graph.append({'indices': puzzle_input.start, 'edges': []})
-    while len(stack) > 0:
-        curr_node = stack.pop()
-        for dir in search_dirs:
-            search_char = curr
+
     return puzzle_input
 
-def solve(puzzle_input):
+def cost_heuristic(puzzle_input: PuzzleInput, current_node: Node) -> int:
+    num_turns = 0
+    # calculate the minimal turns required to get to the goal in a maze with no walls
+    if current_node['orientation'] == 'R':
+        # form a backwards L, 1 turn required
+        num_turns = 1
+    elif current_node['orientation'] == 'U':
+        # forms an upside down L, 1 turn required
+        num_turns = 1
+    # corner case where we're on the same row as the end-square
+    if puzzle_input.end[0] == current_node['indices'][0]:
+        return num_turns * 1000 + (puzzle_input.end[1] - current_node['indices'][1])
+    # corner case where we're on the same col as the end-square and facing 
+    if puzzle_input.end[0] == current_node['indices'][0]:
+        return num_turns * 1000 + (puzzle_input.end[1] - current_node['indices'][1])
+
+def solve(puzzle_input: PuzzleInput) -> None:
     solution = None
     print(f"Solution: {solution}")
