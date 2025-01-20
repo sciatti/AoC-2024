bce14b40e21d04e5912b3cab72cbdd6359e4338d
sciatti <sciatti@umich.edu>
2025-01-19 21:38:47 -0500

    adding some boilerplate to the template

--- template/puzzle1/sln.py
+++ template/puzzle1/sln.py
@@ -1,7 +1,13 @@
-def parse_input(file_handler):
+from __future__ import annotations
+from dataclasses import dataclass
+
+@dataclass
+class PuzzleInput():
+
+def parse_input(file_handler) -> PuzzleInput:
     puzzle_input = None
     return puzzle_input
 
-def solve(puzzle_input):
+def solve(puzzle_input: PuzzleInput):
     solution = None
     print(f"Solution: {solution}")
